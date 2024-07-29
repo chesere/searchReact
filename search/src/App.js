c6dac307af686c469bfb53a93c7fed20985fe97c
chesere <chessm755@gmail.com>
2024-07-29 13:17:35 +0300

    Created js object and set the search

--- search/src/App.js
+++ search/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import {data} from './data.js';
 
 function App() {
+  const [search,setSearch] = useState('');
+  console.log(search);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form >
+        <input type="text" placeholder='search' onChange={e => {
+          setSearch(e.target.value);
+        }}/>
+        
+
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item =>(
+            <tr key={item.id}>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td>{item.phone}</td>
+              </tr>
+
+
+          ))}
+
+        </tbody>
+      </table>
+      
     </div>
   );
 }
